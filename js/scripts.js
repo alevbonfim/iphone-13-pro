bc61a5c43c85333f6a3f51139c3034245d44e473
Alexsandra <alexsandra.bonfim.vieira@gmail.com>
2023-12-06 14:37:16 -0300

    criando select de cores

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,35 @@
 document.getElementById('green').onclick = function(){
     var image = this.dataset.image;
     document.getElementById('product-image').src = image;
+    document.getElementsByClassName('selected')[0].classList.remove('selected');
+    document.querySelector('#green .color').classList.add('selected');
+}
+
+document.getElementById('silver').onclick = function(){
+    var image = this.dataset.image;
+    document.getElementById('product-image').src = image;
+    document.getElementsByClassName('selected')[0].classList.remove('selected');
+    document.querySelector('#silver .color').classList.add('selected');
+
+}
+
+document.getElementById('golden').onclick = function(){
+    var image = this.dataset.image;
+    document.getElementById('product-image').src = image;
+    document.getElementsByClassName('selected')[0].classList.remove('selected');
+    document.querySelector('#golden .color').classList.add('selected');
+}
+
+document.getElementById('grafite').onclick = function(){
+    var image = this.dataset.image;
+    document.getElementById('product-image').src = image;
+    document.getElementsByClassName('selected')[0].classList.remove('selected');
+    document.querySelector('#grafite .color').classList.add('selected');
+}  
+
+document.getElementById('blue').onclick = function(){
+    var image = this.dataset.image;
+    document.getElementById('product-image').src = image;
+    document.getElementsByClassName('selected')[0].classList.remove('selected');
+    document.querySelector('#blue .color').classList.add('selected');
 }
