8dc28d9bfd66ab46e47812417a9515230cfea709
Alexsandra <alexsandra.bonfim.vieira@gmail.com>
2023-12-19 13:37:58 -0300

    minimizando cod js

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,12 @@
-document.getElementById('green').onclick = function(){
-    var image = this.dataset.image;
-    document.getElementById('product-image').src = image;
-    document.getElementsByClassName('selected')[0].classList.remove('selected');
-    document.querySelector('#green .color').classList.add('selected');
-}
+var cores = document.getElementsByClassName('opt-color');
 
-document.getElementById('silver').onclick = function(){
-    var image = this.dataset.image;
-    document.getElementById('product-image').src = image;
-    document.getElementsByClassName('selected')[0].classList.remove('selected');
-    document.querySelector('#silver .color').classList.add('selected');
+Array.from(cores).forEach(function(cor){
+    cor.addEventListener('click', function(){
+        var image = this.dataset.image;
+        document.getElementById('product-image').src = image;
 
-}
+        document.getElementsByClassName('selected')[0].classList.remove('selected');
+        this.children[0].classList.add('selected');
+    });
 
-document.getElementById('golden').onclick = function(){
-    var image = this.dataset.image;
-    document.getElementById('product-image').src = image;
-    document.getElementsByClassName('selected')[0].classList.remove('selected');
-    document.querySelector('#golden .color').classList.add('selected');
-}
-
-document.getElementById('grafite').onclick = function(){
-    var image = this.dataset.image;
-    document.getElementById('product-image').src = image;
-    document.getElementsByClassName('selected')[0].classList.remove('selected');
-    document.querySelector('#grafite .color').classList.add('selected');
-}  
-
-document.getElementById('blue').onclick = function(){
-    var image = this.dataset.image;
-    document.getElementById('product-image').src = image;
-    document.getElementsByClassName('selected')[0].classList.remove('selected');
-    document.querySelector('#blue .color').classList.add('selected');
-}
+})
